6e2d1e876a9e3fcf427dfb306654f9f4b48a292c
alicedubrovskaya <alicedubrovskaya@gmail.com>
2020-09-23 12:48:33 +0300

    Created class that implements time assignment

---
+++ task02сonsole/src/by/training/06time/TimeAssignment.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class TimeAssignment {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your last name: ");
+        String lastName = in.nextLine();
+        System.out.print("Enter count of days you need to do this task: ");
+        int neededDays = in.nextInt();
+
+        Calendar calendar = new GregorianCalendar();
+        Date currentTime = calendar.getTime();
+        System.out.println("Dear "+lastName+"\nThe current time is: "+ currentTime);
+
+
+        calendar.add(Calendar.DATE, neededDays);
+        System.out.println("Deadline: "+calendar.getTime());
+    }
+}
