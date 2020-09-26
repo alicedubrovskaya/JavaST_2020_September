329a672280ff81de6486c5bd49853adb69e8eebb
alicedubrovskaya <alicedubrovskaya@gmail.com>
2020-09-26 19:04:29 +0300

    Added to tests description what is being checked

--- task02сonsole/src/by/training/06time/TimeAssignment.java
+++ task02сonsole/src/by/training/06time/TimeAssignment.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class TimeAssignment {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter your last name: ");
-        String lastName = in.nextLine();
+        System.out.print("Enter your surname: ");
+        String surname = in.nextLine();
         System.out.print("Enter count of days you need to do this task: ");
         short neededDays = in.nextShort();
 
         Calendar calendar = new GregorianCalendar();
         Date currentTime = calendar.getTime();
-        System.out.println("Dear "+lastName+"\nThe current time is: "+ currentTime);
+        System.out.println("Dear "+surname+"\nThe current time is: "+ currentTime);
 
 
         calendar.add(Calendar.DATE, neededDays);
